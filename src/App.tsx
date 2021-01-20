cc5b0d2338a2f05d78684201faa098c7c18b280d
Jesse Clark <hello@developerjesse.com>
2021-01-20 18:09:01 +0200

    Add basic RIF branding.
    - header/content/footer
    - update tests to match

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.scss'
+import rifCredentialViewerLogo from './assets/images/rif-credential-viewer.svg'
+import poweredByRif from './assets/images/powered-by-iov.svg'
+import './assets/scss/_index.scss'
+import { version } from '../package.json'
 
 function App () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container header">
+        <div className="column">
+          <img src={rifCredentialViewerLogo} alt="RIF Credential Viewer" />
+        </div>
+      </div>
+      <div className="container content">
+        <div className="column">
+          Content
+        </div>
+      </div>
+      <div className="container footer">
+        <div className="column">
+          <img src={poweredByRif} alt="Powered By RIF" />
+          <p>Copyright &copy; 2021 IOV Labs. All rights reserved.</p>
+          <p>Version {version}</p>
+        </div>
+      </div>
     </div>
   )
 }
